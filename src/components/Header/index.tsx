b3d13b367efbc2f2870c49757e714bb42d574d6e
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-08-31 18:27:48 -0300

    fetch posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
-export default function Header() {
-  // TODO
+import { ReactElement } from 'react';
+
+import styles from './header.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/">
+          <img src="/images/Logo.svg" alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,83 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }: Post) {
+  return (
+    <>
+      <Head>
+        <title>Home | News.JS</title>
+      </Head>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+// export const getStaticProps: GetStaticProps = async () => {
+//   const prismic = getPrismicClient();
 
-//   // TODO
+//   const postsResponse = await prismic.query(
+//     [Prismic.predicates.at('document.type', 'post')],
+//     {
+//       fetch: ['post.title', 'post.content'],
+//       fetchLinks: ['author.name'],
+//       pageSize: 20,
+//     }
+//   );
+
+//   const posts = postsResponse.results.map(post => {
+//     return {
+//       slug: post.uid,
+//       // title: post.data.title,
+//       updateAt: new Date(post.first_publication_date).toLocaleString('pt-BR', {
+//         day: '2-digit',
+//         month: 'short',
+//         year: 'numeric',
+//       }),
+//       data: {
+//         title: post.data.title,
+//         subtitle: post.data.subtitle,
+//         author: post.data.author,
+//       },
+//     };
+//   });
+//   // console.log(postsResponse);
+//   console.log(posts);
+//   return {
+//     props: { posts },
+//   };
 // };
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query('', {
+    pageSize: 2,
+    ref: previewData?.ref ?? null,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  console.log(JSON.stringify(postsResponse));
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+      preview,
+    },
+  };
+};
